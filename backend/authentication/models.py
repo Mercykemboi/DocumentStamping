2f9e66f1fe2d6a38dd4f8382bee87359ee9fc56c
MercyKemboi <kemboimercy49@gmail.com>
2025-01-14 22:11:24 +0300

    feature: added the authentication functionality for registration,login and logout

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    # Add any additional fields here if needed
+   # phone_number = models.CharField(max_length=15, blank=True, null=True)
+   # address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
