ad8d102bff1feedaf79e7e3ea40eee4bdf10b794
MercyKemboi <kemboimercy49@gmail.com>
2025-01-28 22:58:40 +0300

    fix: pepstandard

---
+++ backend/backend/documents/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+
+from .views import UploadDocumentView, DocumentDetailView
+from .views import StampDocumentView, UserDocumentsView
+
+urlpatterns = [
+    path('documents/',
+         UploadDocumentView.as_view(),
+         name='upload_document'),
+    path('viewDocuments/',
+         UserDocumentsView.as_view(),
+         name='user-documents'),
+    path('documents/<int:pk>/',
+         DocumentDetailView.as_view(),
+         name='document_detail'),
+    path('documents/<int:pk>/stamp/',
+         StampDocumentView.as_view(),
+         name='stamp_document'),]
---
+++ backend/backend/stamps/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Stamp
+
+
+class StampSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stamp
+        fields = '__all__'
